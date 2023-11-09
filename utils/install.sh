236964b9c012af27175be65a816877e08d031480
Nicolò Diamante <hello@nicolodiamante.com>
2023-11-09 19:07:28 +0100

    refined bootstrap and streamlined install/uninstall scripts for better performance and reliability

--- utils/install.sh
+++ utils/install.sh
@@ -10,39 +10,54 @@ ENV_VAR="${TARGET}/.env"
 
 # Check for Homebrew, if not found then install.
 echo 'Checking for Homebrew...'
-if ! command -v brew &> /dev/null; then
+if ! command -v brew &>/dev/null; then
   echo 'Homebrew is missing! Installing it...'
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo 'Updating Homebrew...'
+  brew update
 fi
 
 # Check for Node and npm, if not found then install.
-if ! command -v node &> /dev/null; then
+if ! command -v node &>/dev/null; then
   echo 'Node is missing! Installing it...'
   brew install node
 fi
 
 # Check for tmux, if not found then install.
-if ! command -v tmux &> /dev/null; then
+if ! command -v tmux &>/dev/null; then
   echo 'Tmux is missing! Installing it...'
   brew install tmux
 fi
 
-# Install dependencies via npm.
-if ! command -v npm &> /dev/null; then
-  echo 'There was an error initializing the project with npm.'
+# Check for npm, if not found then exit.
+if ! command -v npm &>/dev/null; then
+  echo 'npm is not installed and is required for the project.' >&2
   exit 1
+fi
+
+# Navigate to the target directory.
+echo 'Installing dependencies via npm...'
+if ! cd "${TARGET}"; then
+  echo "Error: Failed to navigate to ${TARGET}." >&2
+  exit 1
+fi
+
+# Install dependencies via npm.
+echo 'Initializing npm project and installing dependencies...'
+if npm init -y &>/dev/null && npm install; then
+  echo 'Npm project initialized and dependencies installed successfully.'
 else
-  echo 'Installing dependencies via npm...'
-  cd "${TARGET}" && npm init -y &> /dev/null && npm install
+  echo "Failed to initialize npm project or install dependencies." >&2
+  exit 1
 fi
 
 # Create User Credentials if not present.
-if [ ! -f "${ENV_VAR}" ]; then
+if [[ ! -f "${ENV_VAR}" ]]; then
   cat << EOF > "${ENV_VAR}"
 #
 # Configuration for MAIA - Discord Chatbot.
 #
-
 # Discord Settings
 DISCORD_TOKEN=                    # Bot authentication token
 DISCORD_CHANNEL_ID=               # Optional: Restrict bot to this channel
--- utils/uninstall.sh
+++ utils/uninstall.sh
@@ -10,18 +10,22 @@ ENV_VAR="${TARGET}/.env"
 
 # Ask for user confirmation before proceeding.
 echo "This will uninstall MAIA dependencies, Node, npm, and Tmux if confirmed."
-read -p "Are you sure you want to proceed? (y/N) " confirm
-if [[ "$confirm" != "y" && "$confirm" != "Y" ]]; then
+read -q "REPLY?Are you sure you want to proceed? [y/N] "
+echo ""
+if [[ ! "$REPLY" =~ ^[Yy]$ ]]; then
   echo "Uninstallation aborted."
   exit 0
 fi
 
 # Delete the .env file (optional and should ask the user if they want to).
 if [[ -f "${ENV_VAR}" ]]; then
-  read -p "Do you want to delete the .env file? This cannot be undone. (y/N) " delete_env
-  if [[ "$delete_env" == "y" || "$delete_env" == "Y" ]]; then
+  read -q "REPLY?Do you want to delete the .env file? This cannot be undone. [y/N] "
+  echo ""
+  if [[ "$REPLY" =~ ^[Yy]$ ]]; then
     rm -v "${ENV_VAR}"
     echo "Deleted the .env file."
+  else
+    echo "The .env file was not deleted."
   fi
 fi
 
@@ -29,35 +33,43 @@ fi
 if [[ -d "${TARGET}/node_modules" ]]; then
   echo "Uninstalling npm dependencies..."
   if ! cd "${TARGET}"; then
-    echo "Failed to navigate to ${TARGET}. Uninstallation aborted."
+    echo "Failed to navigate to ${TARGET}. Uninstallation aborted." >&2
     exit 1
   fi
-  npm uninstall $(npm list --parseable --depth=0 | awk -F/ '{print $NF}' | tr '\n' ' ') || {
-    echo "Failed to uninstall npm dependencies."
+
+  echo "Attempting to uninstall all npm dependencies in the project..."
+  if ! npm uninstall $(npm list --parseable --depth=0 | awk -F/ '{print $NF}' | tr '\n' ' '); then
+    echo "Failed to uninstall npm dependencies." >&2
     exit 1
-  }
-  echo "Dependencies removed."
+  else
+    echo "Npm dependencies successfully uninstalled."
+  fi
 fi
 
 # Optionally remove Node, npm, and tmux.
-read -p "Do you want to uninstall Node, npm, and Tmux? This might affect other projects. (y/N) " uninstall_apps
-if [[ "$uninstall_apps" == "y" || "$uninstall_apps" == "Y" ]]; then
+read -q "REPLY?Do you want to uninstall Node, npm, and Tmux? This might affect other projects. [y/N] "
+echo ""
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
   # Uninstall Node and npm.
-  if command -v node &> /dev/null; then
+  if command -v node &>/dev/null; then
     echo 'Uninstalling Node and npm...'
-    brew uninstall node || {
-      echo "Failed to uninstall Node."
+    if ! brew uninstall node; then
+      echo "Failed to uninstall Node." >&2
       exit 1
-    }
+    else
+      echo "Node and npm uninstalled successfully."
+    fi
   fi
 
   # Uninstall tmux.
-  if command -v tmux &> /dev/null; then
+  if command -v tmux &>/dev/null; then
     echo 'Uninstalling Tmux...'
-    brew uninstall tmux || {
-      echo "Failed to uninstall Tmux."
+    if ! brew uninstall tmux; then
+      echo "Failed to uninstall Tmux." >&2
       exit 1
-    }
+    else
+      echo "Tmux uninstalled successfully."
+    fi
   fi
 else
   echo "Node, npm, and Tmux remain installed."
